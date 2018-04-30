ca67adc1bb0cdd62d2b1a4544ec0bc82e3eb84c6
Jorde Kang <jorde@protonmail.ch>
2018-04-30 21:29:33 +0100

    updated

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "configuration.org"
@@ -7,12 +14,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-plus-contrib exwm))))
+ '(custom-safe-themes
+   (quote
+    ("95b1450c6a38a211a53fd28c1dcf242b31fcf75d394579e0b11c853423388488" "8bceed439b6d46e0234e0be965cc4d2dc899786d4ce37fbaf10fede43b1cdf79" default)))
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (powerline org-bullets telephone-line magit org-plus-contrib exwm)))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "GohuFont" :foundry "Gohu" :slant normal :weight bold :height 105 :width normal))))
+ '(default ((t (:family "Iosevka" :foundry "CYEL" :slant normal :weight normal :height 120 :width normal))))
  '(hl-line ((t (:background "darkblue" :foreground "white")))))
 
